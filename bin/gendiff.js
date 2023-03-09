ce84aedca87ede3141faa4b14a5b5901d84d5461
Vladislav Kostyuk <logos535@gmail.com>
2023-03-10 00:04:30 +0300

    working on step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,32 @@
-import { program } from 'commander';
+#!/usr/bin/env node
 
+import { program } from 'commander';
+import getDiff from '../src/getDiff.js';
 
 program
 .name('gendiff')
+.version('1.0.0', '-v, --vers', 'output the current version')
+.helpOption('-h, --help', 'output usage information')
+.option('-f, --format <type>', 'output format')
+.argument('<filePath1>', 'first path to file')
+.argument('<filePath2>', 'second path to file')
 .description('Compares two configuration files and shows a difference.')
-.version('1.0.0')
+.action(( filePath1, filePath2) => {
+  // var config = require(filePath1);
+  // var config2 = require(filePath2);
+  // const parsedFirst =  JSON.parse(filePath1);
+  // const parsedSecond =  JSON.parse(filePath2);
+  console.log('filePath1:', filePath1)
+  console.log('filePath2:', filePath2)
 
-program.option('-h, --help', 'info about programm')
+
+
+  const genDiff = (filePath1, filePath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  }
+  return genDiff(filePath1, filePath2)
+});
 
 program.parse(process.argv);
 
 const options = program.opts();
-if (options.help) console.log(`Usage: gendiff [options]
-
-Compares two configuration files and shows a difference.
-
-Options:
-  -V, --version        output the version number
-  -h, --help           display help for command`)
---
+++ src/getDiff.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import parser from './parser.js';
+
+const getDiff = (filePath1, filePath2) => {
+    
+}
